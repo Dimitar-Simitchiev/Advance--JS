bead2ce5dbbcca78ebbf5955654f7f08e86d0c29
Dimitar Simitchiev <51047171+Dimitar-Simitchiev@users.noreply.github.com>
2022-12-16 14:17:34 +0200

    Add files via upload

---
+++ ClassJS/PointDistance.js
@@ -0,0 +1,27 @@
+class Point{
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+
+    }
+    static distance(fisrt,second) {
+        let a =Math.abs(fisrt.x-second.x);
+        let b=Math.abs(fisrt.y-second.y);
+        let distance=Math.sqrt(a**2+b**2);
+        return distance
+
+    }
+
+}
+
+
+
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
+
+
+
+
+
